4b566c6af22b57cd74fb3fc552d5e49cf18e7dbb
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-05-07 19:48:34 +0430

    connecting server / .env example file

--- server/index.js
+++ server/index.js
@@ -9,5 +9,14 @@ const PORT = process.env.PORT || 5000;
 App.use(express.json());
 App.use(cors());
 
+mongoose.connect(
+    process.env.DB,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    },
+    ()=> console.log('db connected')
+);
+
 
 App.listen(PORT , ()=> console.log(`server started on port: ${PORT}`));
