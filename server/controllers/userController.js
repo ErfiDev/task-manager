b8632ec3095fedeacabe55125e4aa2102f35e779
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-05-10 21:51:23 +0430

    add task api complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,85 +1,152 @@
-const model = require('../models/userModel');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const {registerValidation,loginValidation} = require('../validations/userValidation');
+const model = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {
+  registerValidation,
+  loginValidation,
+  taskValidation,
+} = require("../validations/userValidation");
 
-async function register(req , res) {
-    let {error} = await registerValidation(req.body);
-    if(error){
-        return res.json({
-            msg: error.details[0].message,
-            status: 406
-        });
+async function register(req, res) {
+  let { error } = await registerValidation(req.body);
+  if (error) {
+    return res.json({
+      msg: error.details[0].message,
+      status: 406,
+    });
+  } else {
+    const findByUser = await model.find({ username: req.body.username });
+    if (findByUser.length > 0) {
+      return res.json({
+        msg: "this username already used!",
+        status: 406,
+      });
     }
-    else{
-        const findByUser = await model.find({username: req.body.username});
-        if(findByUser.length > 0){
-            return res.json({
-                msg: 'this username already used!',
-                status: 406
-            });
-        }
 
-        const encryptPass = await bcrypt.hash(req.body.password , 10);
-        const body = {
-            username: req.body.username,
-            password: encryptPass,
-            tasks: req.body.tasks,
-            isAdmin: req.body.isAdmin
-        }
-        const data = await new model(body);
-        data.save()
-        .then(r => { res.json({status: 201}) })
-        .catch(er => { res.json({status: 406 , msg: er}) });
-    }
+    const encryptPass = await bcrypt.hash(req.body.password, 10);
+    const body = {
+      username: req.body.username,
+      password: encryptPass,
+      tasks: req.body.tasks,
+      isAdmin: req.body.isAdmin,
+    };
+    const data = await new model(body);
+    data
+      .save()
+      .then((r) => {
+        res.json({ status: 201 });
+      })
+      .catch((er) => {
+        res.json({ status: 406, msg: er });
+      });
+  }
 }
 
-async function login(req , res) {
-    let {error} = await loginValidation(req.body);
-    if(error){
+async function login(req, res) {
+  let { error } = await loginValidation(req.body);
+  if (error) {
+    return res.json({
+      msg: error.details[0].message,
+      status: 406,
+    });
+  } else {
+    const findByUser = await model.findOne({ username: req.body.username });
+    if (!findByUser) {
+      return res.json({
+        msg: "cant find user with this username",
+        status: 406,
+      });
+    } else {
+      let comparePass = await bcrypt.compare(
+        req.body.password,
+        findByUser.password
+      );
+      if (!comparePass) {
         return res.json({
-            msg: error.details[0].message,
-            status: 406
+          msg: "password dont match!",
+          status: 406,
         });
-    }else{
-        const findByUser = await model.findOne({username: req.body.username});
-        if(!findByUser){
-            return res.json({
-                msg: 'cant find user with this username',
-                status: 406
-            });
-        }
-        else{
-            let comparePass = await bcrypt.compare(
-                req.body.password , findByUser.password
-            );
-            if(!comparePass){
-                return res.json({
-                    msg: 'password dont match!',
-                    status: 406
-                });
-            }
+      }
 
-            const today = new Date();
-            const tomorrow = new Date(today);
-            tomorrow.setDate(today.getDate() + 1);
-            
-            const token = await jwt.sign(
-                {token: findByUser} , process.env.ACCESS_TOKEN
-            );
-            
-            res.json({
-                status: 200,
-                data: {
-                    token,
-                    exp: tomorrow
-                }
-            })
-        }
+      const today = new Date();
+      const tomorrow = new Date(today);
+      tomorrow.setDate(today.getDate() + 1);
+
+      const token = await jwt.sign(
+        { token: findByUser },
+        process.env.ACCESS_TOKEN
+      );
+
+      res.json({
+        status: 200,
+        data: {
+          token,
+          exp: tomorrow,
+        },
+      });
     }
+  }
+}
+
+async function addTask(req, res) {
+  let { uuid } = req.params;
+  if (!uuid) {
+    return res.json({
+      msg: "please complete required parametr",
+      status: 406,
+    });
+  } else {
+    let find = await model.findOne({ uuid });
+    if (!find) {
+      return res.json({
+        msg: "cannot find user with this uuid",
+        status: 200,
+      });
+    } else {
+      let { error } = await taskValidation(req.body);
+      if (error) {
+        return res.json({
+          msg: error.details[0].message,
+          status: 406,
+        });
+      } else {
+        let { endTime, title, status } = req.body;
+        if (!endTime) {
+          await model.updateOne(
+            { uuid },
+            {
+              $push: {
+                tasks: { title, status },
+              },
+            }
+          );
+
+          res.json({
+            msg: "add task successful!",
+            status: 200,
+          });
+        } else {
+            await model.updateOne(
+                { uuid },
+                {
+                  $push: {
+                    tasks: { title, status , endTime },
+                  },
+                }
+              );
+
+          res.json({
+            msg: "add task successful!",
+            status: 200,
+          });
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
-    register,
-    login
-}
+  register,
+  login,
+  addTask,
+};
--- server/routes/userApi.js
+++ server/routes/userApi.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const route = express.Router();
-const { register , login } = require('../controllers/userController');
+const { register , login , addTask } = require('../controllers/userController');
 
 route.post('/register' , register);
-route.post('/login' , login)
+route.post('/login' , login);
+route.post('/addTask/:uuid?' , addTask)
 
 module.exports = route;
