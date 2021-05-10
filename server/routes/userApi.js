098f867c081aae4cdaea8eb6a2b5b9f395d4d737
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-05-10 22:06:12 +0430

    edit and delete task api route

--- server/routes/userApi.js
+++ server/routes/userApi.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const { register , login , addTask } = require('../controllers/userController');
+const {
+  register,
+  login,
+  addTask,
+  editTask,
+  deleteTask,
+} = require("../controllers/userController");
 
-route.post('/register' , register);
-route.post('/login' , login);
-route.post('/addTask/:uuid?' , addTask)
+route.post("/register", register);
+route.post("/login", login);
+route.post("/addTask/:uuid?", addTask);
+route.post("/editTask/:uuid?", editTask);
+route.post("/deleteTask/:uuid?", deleteTask);
 
 module.exports = route;
