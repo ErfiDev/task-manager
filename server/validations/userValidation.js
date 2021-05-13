467bc17e3e4c1dc9f51295b21a8e555474556373
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-05-13 14:40:48 +0430

    refactoring code / 3

--- server/validations/userValidation.js
+++ server/validations/userValidation.js
@@ -1,55 +1,54 @@
-const joi = require('joi');
+const joi = require("joi");
 
-const registerValidation = (data)=>{
-    const schema = joi.object({
-        username: joi.string().min(4).max(30).required(),
-        password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
-        .min(8),
-        tasks: joi.array().items(
-            joi.object({
-                title: joi.string().required(),
-                status: joi.boolean().required(),
-                endTime: joi.date().min('now')
-            })
-        ),
-        isAdmin: joi.boolean().required()
-    });
-
-    return schema.validate(data);
-}
-
-const loginValidation = (data)=>{
-    const schema = joi.object({
-        username: joi.string().min(4).max(30).required(),
-        password: joi.string().min(8).required()
-    });
-
-    return schema.validate(data);
-}
-
-const taskValidation = (data)=>{
-    const schema = joi.object({
+const registerValidation = (data) => {
+  const schema = joi.object({
+    username: joi.string().min(4).max(30).required(),
+    password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).min(8),
+    tasks: joi.array().items(
+      joi.object({
         title: joi.string().required(),
         status: joi.boolean().required(),
-        endTime: joi.date().min('now')
-    });
+        endTime: joi.date().min("now"),
+      })
+    ),
+    isAdmin: joi.boolean().required(),
+  });
 
-    return schema.validate(data);
-}
+  return schema.validate(data);
+};
 
-const editTaskValidation = (data)=>{
-    const schema = joi.object({
-        title: joi.string(),
-        status: joi.boolean(),
-        endTime: joi.date().min('now')
-    });
+const loginValidation = (data) => {
+  const schema = joi.object({
+    username: joi.string().min(4).max(30).required(),
+    password: joi.string().min(8).required(),
+  });
 
-    return schema.validate(data);
-}
+  return schema.validate(data);
+};
+
+const taskValidation = (data) => {
+  const schema = joi.object({
+    title: joi.string().required(),
+    status: joi.boolean().required(),
+    endTime: joi.date().min("now"),
+  });
+
+  return schema.validate(data);
+};
+
+const editTaskValidation = (data) => {
+  const schema = joi.object({
+    title: joi.string(),
+    status: joi.boolean(),
+    endTime: joi.date().min("now"),
+  });
+
+  return schema.validate(data);
+};
 
 module.exports = {
-    registerValidation,
-    loginValidation,
-    taskValidation,
-    editTaskValidation
-}
+  registerValidation,
+  loginValidation,
+  taskValidation,
+  editTaskValidation,
+};
