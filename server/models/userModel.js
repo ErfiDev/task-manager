aba2b25fb714cd2f6f26d0ca59e1beae004eba10
ErfanhanifezadeDeveloper <erfanhanifezade@gmail.com>
2021-05-13 14:40:12 +0430

    refactoring code / 2

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const schema = mongoose.Schema;
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const userModel = new schema({
-    username: {type: String , required: true},
-    password: {type: String , required: true},
-    joinedDate: 
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  joinedDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  tasks: [
     {
-        type: Date , 
-        default: Date.now()
+      title: { type: String, required: true },
+      status: { type: Boolean, required: true },
+      endTime: { type: Date, required: false },
+      uuid: { type: String, default: () => uuidv4() },
     },
-    tasks: [{
-        title: {type: String , required: true},
-        status: {type: Boolean , required: true},
-        endTime: {type: Date , required: false},
-        uuid: {type: String , default: ()=> uuidv4()}
-    }],
-    uuid: {type: String , default: ()=> uuidv4()},
-    isAdmin: {type: Boolean , required: true}
+  ],
+  uuid: { type: String, default: () => uuidv4() },
+  isAdmin: { type: Boolean, required: true },
 });
 
-module.exports = mongoose.model('user' , userModel);
+module.exports = mongoose.model("user", userModel);
