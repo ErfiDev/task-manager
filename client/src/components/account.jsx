aba2ae191d06fe3f111f446bb30dbb1d139fe4c1
Erfan <erfanhanifezade@gmail.com>
2021-06-15 22:59:38 +0430

    account component first jsx

--- client/src/components/account.jsx
+++ client/src/components/account.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const Account = () => {
-	return <div className='account'></div>;
+const Account = ({ username, joinedDate, allTask }) => {
+  return (
+    <div className="account">
+      <div className="user-picture"></div>
+      <ul className="user-info">
+        <li>{username}</li>
+        <li>{joinedDate}</li>
+        <li>{allTask}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Account;
--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import SideBar from './sideBar';
-import Main from './main';
-import Login from './login';
-import Register from './register';
-import Admin from './admin';
-import Dashboard from './dashboard';
-import Task from './task';
-import Tasks from './tasks';
-import AddTask from './addTask';
-import Account from './account';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import SideBar from "./sideBar";
+import Main from "./main";
+import Login from "./login";
+import Register from "./register";
+import Admin from "./admin";
+import Setting from "./setting";
+import Task from "./task";
+import Tasks from "./tasks";
+import AddTask from "./addTask";
+import Account from "./account";
 
 const Layout = () => {
-	return (
-		<div className='layout'>
-			<SideBar />
-			<Switch>
-				<Route path='/login' exact component={Login} />
-				<Route path='/register' exact component={Register} />
-				<Route path='/addTask' exact component={AddTask} />
-				<Route path='/allTasks' exact component={Tasks} />
-				<Route path='/task/:uuid' exact component={Task} />
-				<Route path='/setting' exact component={Dashboard} />
-				<Route path='/admin' exact component={Admin} />
-				<Route path='/account' exact component={Account} />
-				<Route path='/' exact component={Main} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div className="layout">
+      <SideBar />
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/addTask" exact component={AddTask} />
+        <Route path="/allTasks" exact component={Tasks} />
+        <Route path="/task/:uuid" exact component={Task} />
+        <Route path="/setting" exact component={Setting} />
+        <Route path="/admin" exact component={Admin} />
+        <Route path="/account" exact component={Account} />
+        <Route path="/" exact component={Main} />
+      </Switch>
+    </div>
+  );
 };
 
 export default Layout;
