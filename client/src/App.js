cfe4ebd19bab03afaf8501d62a78ff4040fa1666
Erfan <erfanhanifezade@gmail.com>
2021-06-15 22:37:23 +0430

    app.js refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React, { Fragment } from 'react';
-import Header from './components/header';
-import Layout from './components/layout';
-import './styles/index.scss';
+import React, { Fragment } from "react";
+import Header from "./components/header";
+import Layout from "./components/layout";
+import "./styles/index.scss";
 
 const App = () => {
-	return (
-		<Fragment>
-			<Header />
-			<Layout />
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <Header />
+      <Layout />
+    </Fragment>
+  );
 };
 
 export default App;
