fd2b84cf85e575baed4ed3b8a54a379114cfff07
Erfan <erfanhanifezade@gmail.com>
2021-06-16 23:15:46 +0430

    add task component complete

--- client/src/components/addTask.jsx
+++ client/src/components/addTask.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 
 const AddTask = () => {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+
+  function submit(e) {
+    e.preventDefault();
+    console.log(title, time);
+  }
+
   return (
     <div className="addTask">
-      <form className="addTask-form">
-        <TextField required label="Task Title" variant="filled" />
+      <form onSubmit={(e) => submit(e)} className="addTask-form">
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+          label="Task Title"
+          variant="filled"
+          autoFocus={true}
+        />
         <TextField
           label="Set Time"
           variant="filled"
           type="date"
-          defaultValue="2020-05-24"
+          defaultValue="2021-05-24"
+          onChange={(e) => setTime(e.target.value)}
         />
         <Button
           className="addTask-form-submit"
           variant="contained"
           color="primary"
+          type="submit"
         >
           Create
         </Button>
