ae217feb86110a258a18b6b74933453a217107a1
Erfan <erfanhanifezade@gmail.com>
2021-06-16 21:38:18 +0430

    sdfg

--- client/src/components/addTask.jsx
+++ client/src/components/addTask.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const AddTask = () => {
-	return <div className='addTask'>add task</div>;
+  return <div className="addTask">add task</div>;
 };
 
 export default AddTask;
