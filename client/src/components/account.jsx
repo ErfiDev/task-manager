c41c5b74960ac4f69415c89aba0d6916b4d905b8
Erfan <erfanhanifezade@gmail.com>
2021-06-16 16:35:57 +0430

    account component/user profile styles complete

--- client/src/components/account.jsx
+++ client/src/components/account.jsx
@@ -4,7 +4,16 @@ import propTypes from "prop-types";
 const Account = ({ username, joinedDate, allTask }) => {
   return (
     <div className="account">
-      <div className="user-picture"></div>
+      <div className="user-picture">
+        <div className="picture-container">
+          <img
+            src="https://images.unsplash.com/photo-1623800184143-bd1c73967392?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80"
+            alt="test"
+            className="profile-picture"
+            title={username + " picture"}
+          />
+        </div>
+      </div>
       <ul className="user-info">
         <li>{username}</li>
         <li>{joinedDate}</li>
