ed57d59922bc07b969588ceeb2221f2f834c5f53
Erfan <erfanhanifezade@gmail.com>
2021-06-16 22:31:19 +0430

    add task component style complete

--- client/src/components/addTask.jsx
+++ client/src/components/addTask.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { TextField, Button } from "@material-ui/core";
 
 const AddTask = () => {
-  return <div className="addTask">add task</div>;
+  return (
+    <div className="addTask">
+      <form className="addTask-form">
+        <TextField required label="Task Title" variant="filled" />
+        <TextField
+          label="Set Time"
+          variant="filled"
+          type="date"
+          defaultValue="2020-05-24"
+        />
+        <Button
+          className="addTask-form-submit"
+          variant="contained"
+          color="primary"
+        >
+          Create
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default AddTask;
