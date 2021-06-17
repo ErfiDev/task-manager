62d65e74b9027fa046d54ce02dc7cdb1ead12797
Erfan <erfanhanifezade@gmail.com>
2021-06-17 17:09:18 +0430

    refactor index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const userRoutes = require('./routes/userApi');
+const userRoutes = require("./routes/userApi");
 
 const App = express();
 const PORT = process.env.PORT || 5000;
@@ -12,14 +12,14 @@ App.use(express.json());
 App.use(cors());
 
 mongoose.connect(
-    process.env.DB,
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    },
-    ()=> console.log('db connected')
+  process.env.DB,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  () => console.log("db connected")
 );
 
-App.use('/api/user' , userRoutes);
+App.use("/api/user", userRoutes);
 
-App.listen(PORT , ()=> console.log(`server started on port: ${PORT}`));
+App.listen(PORT, () => console.log(`server started on port: ${PORT}`));
