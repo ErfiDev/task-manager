4a85424e1c7fabed4e8be83a34d555f3847b3e3e
Erfan <erfanhanifezade@gmail.com>
2021-06-17 15:16:53 +0430

    setting component style complete

--- client/src/components/setting.jsx
+++ client/src/components/setting.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { ChevronRight } from "@material-ui/icons";
 
 const Dashboard = () => {
-	return (
-		<div className='dashboard'>
-			dashboard
-		</div>
-	);
+  return (
+    <div className="dashboard">
+      <ul className="dashboard-list">
+        <li className="dashboard-list-option">
+          Tasks <ChevronRight />
+        </li>
+        <li className="dashboard-list-option">
+          Completed Tasks <ChevronRight />
+        </li>
+        <li className="dashboard-list-option">
+          Uncompleted Tasks <ChevronRight />
+        </li>
+        <li className="dashboard-list-option">
+          Change Password <ChevronRight />
+        </li>
+        <li className="dashboard-list-option">
+          Change Profile <ChevronRight />
+        </li>
+        <li className="dashboard-list-option">
+          Change Username <ChevronRight />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Dashboard;
