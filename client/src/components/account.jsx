0620962db429745e150cca8df42a4d51b700e7d4
Erfan <erfanhanifezade@gmail.com>
2021-06-18 20:23:39 +0430

    fix user information in account component

--- client/src/components/account.jsx
+++ client/src/components/account.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import propTypes from "prop-types";
+import { useSelector } from "react-redux";
 
-const Account = ({ username, joinedDate, allTask }) => {
+const Account = () => {
+  const { token } = useSelector((state) => state.user);
+  let date = new Date(token.joinedDate);
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let joinedDate = [year, month, day].join("-");
+
   return (
     <div className="account">
       <div className="user-picture">
@@ -10,23 +17,17 @@ const Account = ({ username, joinedDate, allTask }) => {
             src="https://images.unsplash.com/photo-1623800184143-bd1c73967392?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80"
             alt="test"
             className="profile-picture"
-            title={username + " picture"}
+            title={token.username + " picture"}
           />
         </div>
       </div>
       <ul className="user-info">
-        <li className="user-info-option info-1">{username}</li>
+        <li className="user-info-option info-1">{token.username}</li>
         <li className="user-info-option info-2">{joinedDate}</li>
-        <li className="user-info-option info-3">{allTask}</li>
+        <li className="user-info-option info-3">{token.tasks.length}</li>
       </ul>
     </div>
   );
-};
-
-Account.propTypes = {
-  username: propTypes.string,
-  joinedDate: propTypes.string,
-  allTask: propTypes.number,
 };
 
 export default Account;
