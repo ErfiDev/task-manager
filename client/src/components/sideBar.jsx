dce31ff971b2fed9f4b8bbaa9ac5a98f296bbfa7
Erfan <erfanhanifezade@gmail.com>
2021-06-18 20:14:19 +0430

    controll user route

--- client/src/components/sideBar.jsx
+++ client/src/components/sideBar.jsx
@@ -8,8 +8,12 @@ import {
 } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 import { sideBar } from "../utils/utils";
+import { useSelector } from "react-redux";
 
 const SideBar = () => {
+  const { token } = useSelector((state) => state.user);
+  const { uuid } = token;
+
   return (
     <div className="side-bar">
       <button className="side-bar-close" onClick={() => sideBar(false)}>
@@ -17,25 +21,25 @@ const SideBar = () => {
       </button>
       <ul className="side-bar-ul">
         <li className="side-bar-ul-li" onClick={() => sideBar(false)}>
-          <Link to="/user/:uuid/addTask">
+          <Link to={`/user/${uuid}/addTask`}>
             <AddCircle fontSize="inherit" color="inherit" />
             &nbsp; Add Task
           </Link>
         </li>
         <li className="side-bar-ul-li" onClick={() => sideBar(false)}>
-          <Link to="/user/:uuid/allTasks">
+          <Link to={`/user/${uuid}/allTasks`}>
             <Assignment fontSize="inherit" color="inherit" />
             &nbsp; All Tasks
           </Link>
         </li>
         <li className="side-bar-ul-li" onClick={() => sideBar(false)}>
-          <Link to="/user/:uuid/account">
+          <Link to={`/user/${uuid}/account`}>
             <Person fontSize="inherit" color="inherit" />
             &nbsp; Account
           </Link>
         </li>
         <li className="side-bar-ul-li" onClick={() => sideBar(false)}>
-          <Link to="/user/:uuid/setting">
+          <Link to={`/user/${uuid}/setting`}>
             <Settings fontSize="inherit" color="inherit" />
             &nbsp; Setting
           </Link>
