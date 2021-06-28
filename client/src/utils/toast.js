805ad675fb09c88081b8e1e4d8e5bda80289a78d
Erfan <erfanhanifezade@gmail.com>
2021-06-28 16:27:45 +0430

    create toast module

---
+++ client/src/utils/toast.js
@@ -0,0 +1,21 @@
+import { toast } from "react-toastify";
+
+export default function Toast(msg, event) {
+  if (event === "success") {
+    return toast.success(msg, {
+      position: "bottom-right",
+      closeOnClick: true,
+    });
+  }
+  if (event === "error") {
+    return toast.success(msg, {
+      position: "bottom-right",
+      closeOnClick: true,
+    });
+  } else {
+    toast(msg, {
+      position: "bottom-right",
+      closeOnClick: true,
+    });
+  }
+}
