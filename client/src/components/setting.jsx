7aebb858cec4c23037a5471ef40b3134cb3aceb0
Erfan <erfanhanifezade@gmail.com>
2021-06-29 21:55:39 +0430

    complete logout user

--- client/src/components/setting.jsx
+++ client/src/components/setting.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import { ChevronRight } from "@material-ui/icons";
 import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import User from "../services/userService";
+import { withRouter } from "react-router-dom";
+import Toast from "../utils/toast";
 
-const Dashboard = ({ match }) => {
+const Dashboard = ({ match, history }) => {
+  async function logout() {
+    try {
+      let { data } = await User.logoutUser(match.params.uuid);
+      if (data.status === 200) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("exp");
+        localStorage.removeItem("loggedIn");
+        Toast("By!", "error");
+        return setTimeout(() => {
+          history.push("/");
+        }, 2500);
+      } else {
+        Toast(data.msg, "error");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="dashboard">
       <ul className="dashboard-list">
@@ -23,11 +45,11 @@ const Dashboard = ({ match }) => {
           </Link>
         </li>
       </ul>
-      <Button variant="contained" color="secondary">
+      <Button onClick={logout} variant="contained" color="secondary">
         Logout
       </Button>
     </div>
   );
 };
 
-export default Dashboard;
+export default withRouter(Dashboard);
